20af850399902aaf0f13882f156c65a58440aa84
safa abid <safaabid.12@gmail.com>
2022-07-01 15:52:02 +0200

    signin et signup commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express=require('express');
 const router =express.Router();
 const userCtrl=require('../controllers/userController');
+const multer = require('../middlewares/multer');
 
 router.get('/',userCtrl.getAllUser);
-router.post('/signup',userCtrl.signup);
+router.post('/signup',multer,userCtrl.signup);
 router.post('/login',userCtrl.login);
 router.delete('/:id',userCtrl.deleteUser)
 
