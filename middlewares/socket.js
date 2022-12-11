651324f7fec2f336f6233f68e38550b9c4a4128b
illimpassa <illimpassa@gmail.com>
2022-12-11 16:48:11 +0100

    socket.io basics done! 12/11/2022

--- middlewares/socket.js
+++ middlewares/socket.js
@@ -9,6 +9,7 @@ module.exports = (io) => {
         if (err) return next(new Error('Authentication error'));
         socket.decoded = decoded;
         socket.userId = decoded.userId;
+        // console.log(socket);
         const user= await User.findOne({_id:socket.userId});
         socket.user = user;
         next();
@@ -44,10 +45,10 @@ module.exports = (io) => {
       const test = MyFriendsIds.includes(elem.userId);
       if(test){
         // send him a notif
-        socket.to(elem.socketId).emit('new user connected',socket.userId);
+        socket.to(elem.socketId).emit('new user connected',socket.id);
       }
     })
-    console.log("connectedUsers: ",connectedUsers);
+    connectedUsers.forEach((elem) => console.log("connectedUsers: ",elem.socket.id));
     /*************************************************/
 
     socket.on("disconnect", (userId) => {
@@ -75,28 +76,52 @@ module.exports = (io) => {
         socket.to(destination).emit("message","from " + socket.userId + " he said : " + data);
     });
 
-    socket.on("join room", async(data) => {
+    socket.on("join private room", async(data) => {
       // MyFriendsIds = socket.user.Matches.map((userId) => userId.toString()) // MyFriendsIds: list of all the matched yours with this socket
       if (MyFriendsIds.includes(data)){
         roomId = [ data, socket.userId].sort().join("");
         socket.join(roomId);
-
+        console.log("1) rooommm: ",roomId);
+        connectedUsers.map((connectedUser) => {
+          console.log("1.1) ",connectedUser.userId == data);
+          if(connectedUser.userId == data){
+            console.log("2) ",socket.id);
+            socket.to(connectedUser.socketId).emit("come join", socket.userId)
+            // connectedUser.socket.join(roomId);
+          }
+        })
       }
 
-        // const sockets = await io.fetchSockets();
-        const sockets = Array.from(io.sockets.sockets).map(socket => socket[0]);
-        console.log("sockets: ",sockets);
-        sockets.forEach((item, index) => {
+        // // const sockets = await io.fetchSockets();
+        // const sockets = Array.from(io.sockets.sockets).map(socket => socket[0]);
+        // // console.log("sockets: ",sockets);
+        // sockets.forEach((item, index) => {
 
+        // })
+        // socket.broadcast.emit('user connected')
+        // // console.log(sockets[0].decoded);
+        // // var clients = io.sockets.clients();
+        // // console.log(clients);
+        // io.to(socket.id).to(socket.id).emit("room entred", {
+        //   message: 'you have entred this room in the data',
+        //   data: data
+        // });
+    });
+
+    socket.on("join room", async(data) => {
+      console.log("3) I am also trying to join ",data);
+      // MyFriendsIds = socket.user.Matches.map((userId) => userId.toString()) // MyFriendsIds: list of all the matched yours with this socket
+      if (MyFriendsIds.includes(data)){
+        roomId = [ data, socket.userId].sort().join("");
+        socket.join(roomId);
+        console.log("4) rom: ",roomId);
+        console.log("5) ",socket.id);
+        connectedUsers.map((connectedUser) => {
+          if(connectedUser.userId == data){
+            socket.join(roomId);
+          }
         })
-        socket.broadcast.emit('user connected')
-        // console.log(sockets[0].decoded);
-        // var clients = io.sockets.clients();
-        // console.log(clients);
-        io.to(socket.id).to(socket.id).emit("room entred", {
-          message: 'you have entred this room in the data',
-          data: roomId
-        });
+      }
     });
 
     socket.on("trying to connect with the new user", (data) => {
@@ -122,5 +147,12 @@ module.exports = (io) => {
         .catch( err => console.log(err));
     })
 
+    socket.on("private message", (result) => {
+      console.log("userId: ",result.userId, " socketId: ",socket.userId);
+      roomId = [ result.userId, socket.userId].sort().join("");
+      console.log("sent to : ",roomId, " message: ",result.msg);
+      io.to(roomId).emit("private message", result.msg)
+    })
   });
+
 }
